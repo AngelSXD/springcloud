377229438a14fc75864c9810887187f0f3a361c7
SXD <sxd4business@qq.com>
2018-11-08 22:52:35 +0800

    更新feign注释

--- springcloud-ms-member/src/main/java/com/swapping/springcloud/ms/member/feign/integral/client/FeignMsIntegralClient.java
+++ springcloud-ms-member/src/main/java/com/swapping/springcloud/ms/member/feign/integral/client/FeignMsIntegralClient.java
@@ -10,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * feignClient 接口
  *
- * 注意：feign接口上 尽量不要使用@RequestMapping()注解
+ * 1.注意：feign接口上 尽量不要使用@RequestMapping()注解
  *      因为若请求中含有Accept header,会出现404
+ *
+ * 2.@FeignClient(name = "springcloud-ms-integral")
+ * 其中的name代表这个feignClient指向的服务名称，
+ * 也就是ms-integral服务中的application.prperties中定义的【spring.application.name=springcloud-ms-integral】
+ *
+ * 3.如果被调用方，也就是服务提供者在服务注册中心有多个实例，则调用方的多次请求会自动轮询到每个服务提供方来处理，
+ * 这就是注册中心的负载均衡的功能
  */
 @FeignClient(name = "springcloud-ms-integral")
 public interface FeignMsIntegralClient {
@@ -21,10 +28,12 @@ public interface FeignMsIntegralClient {
      * jpa分布式事务
      *
      *
-     * 【注意】接口的地址需要完整对应到 被调用方接口的完整地址
+     * 1.【注意】
+     *  接口的地址需要完整对应到 被调用方接口的完整地址
      *  因为feignClient上不推荐直接使用@RequestMapping，
      *  所以将@RequestMapping("/integral")和@RequestMapping("/save")合并到具体接口上的@RequestMapping("/integral/save")
      *
+     * 2.接口的方法名要和被调用接口的方法名一致！！
      * @param entity
      * @return
      */
