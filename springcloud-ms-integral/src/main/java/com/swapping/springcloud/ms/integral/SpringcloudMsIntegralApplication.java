3fea212b96d770e11c87b475f54b8c3a7d4c4256
SXD <sxd4business@qq.com>
2018-11-13 09:34:06 +0800

    spring boot2.0 使用Hystrix Dashboard 必须的注解

--- springcloud-ms-integral/src/main/java/com/swapping/springcloud/ms/integral/SpringcloudMsIntegralApplication.java
+++ springcloud-ms-integral/src/main/java/com/swapping/springcloud/ms/integral/SpringcloudMsIntegralApplication.java
@@ -3,10 +3,14 @@ package com.swapping.springcloud.ms.integral;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableCircuitBreaker	//Hystrix Dashboard必须加
+@EnableHystrixDashboard//展示熔断器仪表盘
 
 @EnableTransactionManagement
 @SpringBootApplication(scanBasePackages = "com.swapping")
--- springcloud-ms-member/src/main/java/com/swapping/springcloud/ms/member/SpringcloudMsMemberApplication.java
+++ springcloud-ms-member/src/main/java/com/swapping/springcloud/ms/member/SpringcloudMsMemberApplication.java
@@ -11,12 +11,14 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableCircuitBreaker	//Hystrix Dashboard必须加
+@EnableHystrixDashboard//展示熔断器仪表盘
 
 @EnableTransactionManagement
-@SpringBootApplication(scanBasePackages = "com.swapping")
 @EnableDiscoveryClient	//启动服务发现与注册
 @EnableFeignClients({"com.swapping"})		//启用feign进行服务之间的远程调用  服务提供者需要这个注解,而服务调用者则不需要这个注解
 @MapperScan("com.swapping.springcloud.ms.member.mapper")//mybatis 的 mapper所在包路径 加了这个注解，则mapper类上不用加@Mapper注解
+@SpringBootApplication(scanBasePackages = "com.swapping")
 public class SpringcloudMsMemberApplication {
 
 	public static void main(String[] args) {
